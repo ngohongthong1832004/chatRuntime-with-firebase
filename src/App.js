7acd9ff6571298999f84078f6c32eb256afe307f
ngohongthong <baphongpine@gmail.com>
2023-02-15 22:48:31 +0700

    r

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+import { database, ref, push, onValue } from './firebase'
+
+function App() {
+
+
+  const [dataFireBase , setDataFireBase] = useState([])
+
+  useEffect(() => {
+    onValue(ref(database, "message"), data => {
+      const arr = []
+      data.forEach((item) => {
+        arr.push(item.val())
+      })
+      console.log(arr)
+    })
+  }, [])
+
+  
+  console.log(dataFireBase)
+
+  return (
+    <div className="App">
+     <h2> ChatRunTime</h2>
+    </div>
+  );
+}
+
+export default App;
