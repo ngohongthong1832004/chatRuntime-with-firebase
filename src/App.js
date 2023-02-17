3f2c2ec4b8421f7cb8bc35b82d13371dcc2a6b4c
ngohongthong <baphongpine@gmail.com>
2023-02-17 23:28:20 +0700

    study fireBase and realtime databse

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { database, ref, push, onValue } from './firebase'
+import moment from 'moment'
 
 function App() {
 
-
+  const [valueMess, setValueMess] = useState('')
   const [dataFireBase , setDataFireBase] = useState([])
+  const inputRef = useRef()
 
-  // useEffect(() => {
-  //   onValue(ref(database, "message"), data => {
-  //     const arr = []
-  //     data.forEach((item) => {
-  //       arr.push(item.val())
-  //     })
-  //     console.log(arr)
-  //   })
-  // }, [])
+  useEffect(() => {
+    onValue(ref(database, 'message'), data => {
+      console.log("data :",data)
+      const arr = []
+      data.forEach((item) => {
+        arr.push(item.val())
+      })
+      setDataFireBase(arr)
+    })
+  }, [])
 
-  
-  console.log(dataFireBase)
+  const handleSendMessage = () => {
+    push(ref(database, 'message'), {
+        name : 'Anonymous',
+        text : valueMess,
+        createAt : moment(Date.now()).format("DD/MM/YYYY")
+    })
+    setValueMess('')
+    inputRef.current.focus()
+  }
+
+  console.log("dataFireBase : ",dataFireBase)
+
 
   return (
     <div className="App">
      <h2> ChatRunTime</h2>
+     <ul>
+      {dataFireBase.map((message, index) => {
+        return <div> 
+            <li key={index} style = {{listStyle:'none'}}>{message.name} : {message.text}</li>
+            <span>{message.createAt}</span>
+           </div>
+      })}
+     </ul>
+     <input ref={inputRef} value={valueMess} onChange = { (e) => setValueMess(e.target.value)  }/>
+     <button onClick={handleSendMessage}>Send Message</button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,34 +1,19 @@
-// Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, push, onValue } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, push, ref, onValue } from 'firebase/database'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC1ATkfWIyhIakBsQHTyGBnRI3raXvR8-A",
   authDomain: "chatruntime-7bf39.firebaseapp.com",
+  databaseURL: "https://chatruntime-7bf39-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "chatruntime-7bf39",
   storageBucket: "chatruntime-7bf39.appspot.com",
   messagingSenderId: "637493477918",
-  appId: "1:637493477918:web:383e15629bf49ac440a40f",
-  measurementId: "G-L0JE63S93Q"
+  appId: "1:637493477918:web:babaeaa10b12bbc740a40f",
+  measurementId: "G-N7YG9LM7GG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
-
-console.log("data : ", onValue(ref(database, "message"), (i) => {
-    console.log(i)
-}))
-onValue(ref(database), (i) => {
-    console.log("i : ",i)
-})
-console.log("database ", database)
 
 export { database, ref, push, onValue }
